3ed82172c573f48997af1d08fee56c4a5c3084c3
VoperYT <92862848+VoperYT@users.noreply.github.com>
2021-11-23 13:42:35 +0100

    Adding a command

--- src/main/java/me/voperyt/testplugin/TestPlugin.java
+++ src/main/java/me/voperyt/testplugin/TestPlugin.java
@@ -1,12 +1,19 @@
 package me.voperyt.testplugin;
 
+import me.voperyt.testplugin.test.CommandTest;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin {
 
+    FileConfiguration config = this.getConfig();
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        // Registering the /test command
+        this.getCommand("test").setExecutor(new CommandTest());
 
     }
 
---
+++ src/main/java/me/voperyt/testplugin/test/CommandTest.java
@@ -0,0 +1,35 @@
+package me.voperyt.testplugin.test;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class CommandTest implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+
+            // Creating a diamond itemStack
+            ItemStack diamond = new ItemStack(Material.DIAMOND);
+            diamond.setAmount(640);
+
+            // Creating a Netherite Sword itemStack
+            ItemStack netheriteSword = new ItemStack(Material.NETHERITE_SWORD);
+
+            // Sharpness 1000 + Fire aspect 50 + Knockback 50
+            netheriteSword.addEnchantment(Enchantment.DAMAGE_ALL, 1000);
+            netheriteSword.addEnchantment(Enchantment.FIRE_ASPECT,50);
+            netheriteSword.addEnchantment(Enchantment.KNOCKBACK,100);
+
+            player.getInventory().addItem(diamond,netheriteSword);
+
+        }
+        return true;
+    }
+}
