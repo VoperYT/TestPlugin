e8456126ce9d9cfb48bea3a2242342e8abd74906
VoperYT <92862848+VoperYT@users.noreply.github.com>
2021-12-01 12:24:25 +0100

    Creating a new PotionEffect

---
+++ src/main/java/me/voperyt/testplugin/test/FluPotion.java
@@ -0,0 +1,12 @@
+package me.voperyt.testplugin.test;
+
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class FluPotion {
+
+    // Potion effect Flu
+    public static final PotionEffect FLU_POTION_EFFECT = new PotionEffect(PotionEffectType.WEAKNESS,60*20,10);
+    
+
+}
